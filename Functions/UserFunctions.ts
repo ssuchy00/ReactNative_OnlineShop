ab8328c91aecf3a65726dc5bb9cdc164d6cfcf82
ssuchy00 <patrykplaypl8@gmail.com>
2025-01-22 17:55:39 +0100

    user functions

---
+++ Functions/UserFunctions.ts
@@ -0,0 +1,11 @@
+import { IUser } from "../Interfaces/IApiResponse"
+
+export const UserFunction = {
+    getUser: ():IUser | null => {
+        return null
+    },
+
+    setUser: (user:IUser) => {
+        
+    }
+}
